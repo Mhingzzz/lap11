9f9f554ffaee8d3d1065d4eec98feb7d796fc7cc
mm <nathaphong_p@cmu.ac.th>
2021-12-17 16:17:37 +0700

    fin monk

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,30 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+
+#include <iostream>
+#include <iomanip>
+#include<fstream>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+    ifstream score("score.txt");
+    string te;
+    double sum,sum2;
+    int i = 0;
+    while (getline(score,te))
+    {
+
+        sum += stod(te);
+        sum2 += pow(stod(te),2);
+        i++;
+    }
+    
+    double mean,divi;
+    mean = sum / i;
+    divi = sqrt((sum2/i)-(pow(mean,2)));   
+
+    cout << "Number of data = "<<i <<endl;
+    cout << setprecision(3);
+    cout << "Mean = "<<mean <<endl;
+    cout << "Standard deviation = "<<divi;
+}
